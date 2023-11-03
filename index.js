c51906e49e77a8e9015b692b8cad22ced8ba1c03
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-11-03 12:54:53 -0300

    body parser e config arquivos estáticos no express

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 
 // View engine
 app.set('view engine', 'ejs');
+
+// Static archives
+app.use(express.static('public'));
+
+// Body parser
+app.use(bodyParser.urlencoded({extends: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.render("index");
