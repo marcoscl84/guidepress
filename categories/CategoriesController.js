22b50e6edd1b20c252bfc3518c6a59cf5b705d5f
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-11-04 15:06:12 -0300

    rota criar nova categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,33 @@
+// import express from "express";
+// const router = express.Router();
+
+// router.get("/admin/categories/new", (req, res) => {
+//     res.render("admin/categories/new");
+// });
+
+// export default router;
+
 import express from "express";
 const router = express.Router();
+import Category from "./Category.js";
+import slugify from "slugify";
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
 });
+
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/");
+        })
+    } else {
+        res.redirect("/admin/categories/new");
+    }
+})
 
 export default router;
