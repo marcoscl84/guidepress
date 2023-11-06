3351cad87eb0893f21c20e06c61bb5f4fa345bfd
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-11-06 10:26:09 -0300

    confirmação exclusão categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -23,17 +23,38 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect("/");
+            res.redirect("/admin/categories/new");
         })
     } else {
         res.redirect("/admin/categories/new");
     }
 });
 
+// Lista todas as categorias
 router.get("/admin/categories", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/categories/index", {categories: categories})
     })
+});
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            })
+        } else {
+            res.redirect("/admin/categories");
+        }
+    } else {
+        res.redirect("/admin/categories");
+    }
 })
 
 export default router;
